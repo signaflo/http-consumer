86c0d9e582be2aab6781f39c894a275d6f9aed10
Jacob Rachiele <jrachiele@bybaxter.com>
2017-03-31 19:50:56 -0500

    Fix Consume IO

--- src/main/java/rest/Consumer.java
+++ src/main/java/rest/Consumer.java
@@ -4,12 +4,8 @@ import lombok.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -31,14 +27,21 @@ public final class Consumer<T> implements Runnable {
     private static final AtomicLong FILE_COUNTER = new AtomicLong();
     private static final DateTimeFormatter DTF = DateTimeFormatter.ISO_LOCAL_DATE;
 
-    private final String url;
+    private final String address;
+    private final URL url;
     private final Class<T> type;
     private String etag = "";
     private RestResponse<T> restResponse = null;
 
-    public Consumer(@NonNull String url, @NonNull Class<T> type) {
-        this.url = url;
+    public Consumer(@NonNull String address, @NonNull Class<T> type) {
+        this.address = address;
         this.type = type;
+        try {
+            this.url = new URL(address);
+        } catch (IOException e) {
+            LOGGER.error("Could not create URL at address: {}", address, e);
+            throw new RuntimeException(e);
+        }
     }
 
     void updateWith(@NonNull final RestRequest<T> restRequest) {
@@ -69,20 +72,21 @@ public final class Consumer<T> implements Runnable {
     public void run() {
         HttpURLConnection connection;
         try {
-            connection = (HttpURLConnection)(new URL(this.url).openConnection());
+            connection = (HttpURLConnection) (this.url.openConnection());
+            connection.setRequestProperty("Content-Type", "text/plain; charset=utf-8");
             connection.setRequestProperty("If-None-Match", this.etag);
             connection.connect();
             RestRequest<T> restRequest = new JavaRestRequest<>(connection);
             this.updateWith(restRequest);
             if (this.restResponse.getStatus() == OK) {
-                writeToFile();
+                writeToFile(connection.getContentLength());
             }
         } catch (IOException e) {
-            LOGGER.error("Could not open connection to {}", url, e);
+            LOGGER.error("Could not open connection to {}", address, e);
         }
     }
 
-    private void writeToFile() {
+    private void writeToFile(int numBytes) {
         String currentDate = LocalDateTime.now().format(DTF);
         String path = currentDate + "_" + FILE_COUNTER.incrementAndGet();
         File file = new File(path);
@@ -92,15 +96,16 @@ public final class Consumer<T> implements Runnable {
             LOGGER.error("The file " + file + " already exists.", e);
             throw e;
         } else {
-            try (FileOutputStream outputStream = new FileOutputStream(file)) {
+            try (BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(
+                    file), numBytes); InputStream inputStream = new BufferedInputStream(
+                    this.restResponse.getBodyAsInputStream(), numBytes)) {
                 file.createNewFile();
-                InputStream inputStream = this.restResponse.getBodyAsInputStream();
-                int streamLength = inputStream.available();
-                byte[] rawData = new byte[streamLength];
-                inputStream.read(rawData);
-                outputStream.write(rawData);
+                byte[] rawData = new byte[numBytes];
+                int len;
+                while ((len = inputStream.read(rawData)) != -1) {
+                    outputStream.write(rawData, 0, len);
+                }
                 outputStream.flush();
-                outputStream.close();
             } catch (IOException ioe) {
                 LOGGER.error("Error writing to output stream.", ioe);
                 throw new RuntimeException("The file " + path + " could not be created.");
@@ -110,8 +115,8 @@ public final class Consumer<T> implements Runnable {
 
     @Override
     public String toString() {
-        return "Consumer{" + "etag='" + etag + '\'' + ", url='" + url + '\'' + ", restResponse=" + restResponse +
-               ", type=" + type + '}';
+        return "Consumer{" + "etag='" + etag + '\'' + ", address='" + address + '\'' + ", restResponse=" +
+               restResponse + ", type=" + type + '}';
     }
 
     @Override
@@ -121,13 +126,13 @@ public final class Consumer<T> implements Runnable {
 
         Consumer<?> that = (Consumer<?>) o;
 
-        if (!url.equals(that.url)) return false;
+        if (!address.equals(that.address)) return false;
         return type.equals(that.type);
     }
 
     @Override
     public int hashCode() {
-        int result = url.hashCode();
+        int result = address.hashCode();
         result = 31 * result + type.hashCode();
         return result;
     }
