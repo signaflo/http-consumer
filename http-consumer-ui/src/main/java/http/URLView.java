e8bfaca33d5c65fab1898722722ae28ad0bdebfe
Jacob Rachiele <jrachiele@bybaxter.com>
2017-06-06 18:09:22 -0500

    Round out URLView elements

--- http-consumer-ui/src/main/java/http/URLView.java
+++ http-consumer-ui/src/main/java/http/URLView.java
@@ -5,7 +5,11 @@ import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.util.StringConverter;
 
 /**
@@ -14,14 +18,51 @@ import javafx.util.StringConverter;
  * @author Jacob Rachiele
  *         Jun. 05, 2017
  */
-public class URLView extends VBox {
+public class URLView extends GridPane {
 
-    public URLView(int padding) {
-        super(padding);
-        this.getChildren().add(getChoiceBox());
+    public URLView() {
+        super();
+        this.setHgap(20);
+        this.setVgap(10);
+        ChoiceBox<StringProperty> choiceBox = getProtocolChoiceBox();
+        Label protocolLabel = getProtocolLabel();
+        protocolLabel.setLabelFor(choiceBox);
+        this.add(protocolLabel, 0, 0);
+        this.add(choiceBox, 1, 0);
+        this.add(getHostLabel(), 0, 1);
+        this.add(getHostTextField(), 1, 1);
+        this.add(getPortLabel(), 0, 2);
+        this.add(getPortTextField(), 1, 2);
+        this.add(getFileLabel(), 0, 3);
+        this.add(getFileTextField(), 1, 3);
     }
 
-    ChoiceBox<StringProperty> getChoiceBox() {
+    private static Label getProtocolLabel() {
+        Label protocolLabel = new Label("Protocol");
+        protocolLabel.setFont(Font.font("SanSerif", FontWeight.BOLD, 14));
+        return protocolLabel;
+    }
+
+    private static Label getHostLabel() {
+        Label hostLabel = new Label("Host");
+        hostLabel.setFont(Font.font("SanSerif", FontWeight.BOLD, 14));
+        return hostLabel;
+    }
+
+    private static Label getPortLabel() {
+        Label portLabel = new Label("Port");
+        //TODO: validate port range.
+        portLabel.setFont(Font.font("Sans Serif", FontWeight.BOLD, 14));
+        return portLabel;
+    }
+
+    private static Label getFileLabel() {
+        Label fileLabel = new Label("File");
+        fileLabel.setFont(Font.font("Sans Serif", FontWeight.BOLD, 14));
+        return fileLabel;
+    }
+
+    private static ChoiceBox<StringProperty> getProtocolChoiceBox() {
         ObservableList<StringProperty> properties = FXCollections.observableArrayList();
         StringProperty httpDefault = new SimpleStringProperty("HTTP");
         properties.add(new SimpleStringProperty("FTP"));
@@ -29,11 +70,30 @@ public class URLView extends VBox {
         properties.add(new SimpleStringProperty("HTTPS"));
         ChoiceBox<StringProperty> choiceBox = new ChoiceBox<>(properties);
         choiceBox.setValue(httpDefault);
-        choiceBox.setConverter(getConverter());
+        choiceBox.setConverter(stringConverter());
         return choiceBox;
     }
 
-    StringConverter<StringProperty> getConverter() {
+    private static TextField getHostTextField() {
+        TextField hostField = new TextField();
+        hostField.setPromptText("host location");
+        return hostField;
+    }
+
+    private static TextField getPortTextField() {
+        TextField portField = new TextField();
+        portField.setText("8081");
+        portField.setPromptText("port number");
+        return portField;
+    }
+
+    private static TextField getFileTextField() {
+        TextField fileTextField = new TextField();
+        fileTextField.setPromptText("file path");
+        return fileTextField;
+    }
+
+    private static StringConverter<StringProperty> stringConverter() {
         return new StringConverter<StringProperty>() {
             @Override
             public String toString(StringProperty object) {
--- http-consumer-ui/src/main/java/http/execution/Main.java
+++ http-consumer-ui/src/main/java/http/execution/Main.java
@@ -11,21 +11,25 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 /**
  * [Insert class description]
  *
  * @author Jacob Rachiele
  *         May. 30, 2017
  */
-public class Main extends Application {
+public final class Main extends Application {
 
     @Override
     public void start(Stage stage) {
         PathPropertiesData data = new PathPropertiesData();
-        Node urlView = new URLView(10);
+        Node urlView = new URLView();
         GridPane root = new GridPane();
         root.add(urlView, 0, 0);
-        Scene scene = new Scene(root, 600, 400);
+        Scene scene = new Scene(root);
+        String slash = File.separator;
+        scene.getStylesheets().add(slash + "url.css");
         stage.setScene(scene);
         stage.show();
     }
